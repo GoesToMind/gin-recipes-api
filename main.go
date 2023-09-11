45934312f0d3ddd03c886094335898ed10b7ce78
Vitor Rodrigues Carvalho <GoesToMind@gmail.com>
2023-09-12 00:33:34 +0100

    recipe data model

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"time"
+)
 
 func main() {
 	router := gin.Default()
@@ -8,4 +11,12 @@ func main() {
 	if err != nil {
 		return
 	}
+}
+
+type Recipe struct {
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
 }
