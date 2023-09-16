f120b55229a94ccc354f5887638c246b085a529c
Vitor Rodrigues Carvalho <GoesToMind@gmail.com>
2023-09-16 11:43:24 +0100

    search recipe by tag

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/rs/xid"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -15,6 +16,7 @@ func main() {
 	router.GET("/recipes", ListRecipesHandler)
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
 	router.DELETE("/recipes/:id", DeleteRecipeHandler)
+	router.GET("recipes/search", SearchRecipesHandler)
 	err := router.Run()
 	if err != nil {
 		return
@@ -106,4 +108,24 @@ func DeleteRecipeHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Recipe has been deleted",
 	})
+}
+
+func SearchRecipesHandler(c *gin.Context) {
+	tag := c.Query("tag")
+	listOfRecipes := make([]Recipe, 0)
+
+	for i := 0; i < len(recipes); i++ {
+		found := false
+
+		for _, t := range recipes[i].Tags {
+			if strings.EqualFold(t, tag) {
+				found = true
+			}
+		}
+		if found {
+			listOfRecipes = append(listOfRecipes, recipes[i])
+		}
+	}
+
+	c.JSON(http.StatusOK, listOfRecipes)
 }
